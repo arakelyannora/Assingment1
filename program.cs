77255315a3a8a8180509e6b9e9f4dd9066c65d9b
arakelyannora <69462842+arakelyannora@users.noreply.github.com>
2020-08-13 17:35:42 +0400

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace firstAssignment
+{
+ class Program
+{
+   static void Main(string[] args)
+	 { 
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+	 }
+  static void Main2()
+	 { 
+		  Console.WriteLine("Hello from Main2!");
+		  Console.ReadLine();
+	 }
+ }
+}
